d6067a51f61cb7424b61129c2c5be8c4148e6540
Matthew Gagnon <matthewgagnon1014@gmail.com>
2024-10-05 21:25:53 -0400

    more variable work in JS

---
+++ JavaScript/dogYears.js
@@ -0,0 +1,16 @@
+// My age
+let myAge = 24;
+// Early years variable
+let earlyYears = 2;
+earlyYears *= 10.5;
+// Later years variable
+let laterYears = myAge - 2;
+// Calculating dog years
+laterYears *= 4
+console.log(`${earlyYears} and ${laterYears}`);
+// My age in dog years 
+let myAgeInDogYears = earlyYears + laterYears;
+// Formatting my name to lower case
+let myName = 'Matt'.toLowerCase();
+// Display all the work
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
